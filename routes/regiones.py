1a414a0c3f822ba5d45d668b0c0e044ed54fae98
terepv <teresa.pegna@gmail.com>
2025-04-05 19:42:58 -0300

    Actualizacion crud region: post

--- routes/regiones.py
+++ routes/regiones.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter, Depends, HTTPException
-from db.models import RegionResponse
-from shared.dependencies import RoleChecker, SyncDbSessionDep
+from typing import Annotated
+from fastapi import APIRouter, Body, Depends, HTTPException
+from db.models import Region, RegionResponse
+from shared.dependencies import RoleChecker, SyncDbSessionDep, get_user_from_token_data
 from shared.enums import RolesEnum
+from shared.schemas import RegionCreate, UsuarioOut
+from shared.utils import get_example, get_local_now_datetime
 
 router = APIRouter(prefix="/regiones", tags=["Regiones"])
 
@@ -48,3 +51,48 @@ def read_region(
     if not region:
         raise HTTPException(status_code=404, detail="No existe región con ese id")
     return region
+
+
+@router.post(
+    "/",
+    response_model=RegionResponse,
+    status_code=201, 
+    response_model_exclude_none=True,
+    summary="Crear una nueva región",
+)
+def add_region(
+    db: SyncDbSessionDep,
+    user: Annotated[UsuarioOut, Depends(get_user_from_token_data)],
+    region: RegionCreate = Body(
+        openapi_examples={
+            "default": get_example("region_post"),
+        }
+    ),
+    _: bool = Depends(RoleChecker(allowed_roles=[RolesEnum.ADMIN])),
+):
+
+
+    """
+    Crea una nueva región.
+
+    Argumentos:
+    - nombre del plan (str)
+
+    Devuelve un mensaje de confirmación con el recurso actualizado.
+    Requiere rol de Administrador.
+    """
+
+    if db.query(Region).filter(Region.nombre.ilike(region.nombre)).first():
+        raise HTTPException(status_code=409, detail="Región ya existe")
+
+    data = RegionResponse(
+        region=region.nombre,
+        fecha_creacion=get_local_now_datetime(),
+        creado_por=user.email,
+    )
+
+    db.add(data)
+    db.commit()
+    db.refresh(data)
+
+    return {"message": "Se creó una región", "región": data}
