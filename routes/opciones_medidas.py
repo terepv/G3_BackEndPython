8851dcf6048654c1b8dd7e8802d29bf0bfc239c2
hildasilva <hilda0104@gmail.com>
2025-04-06 22:09:27 -0400

    POST de opciones_medidas

--- routes/opciones_medidas.py
+++ routes/opciones_medidas.py
@@ -4,7 +4,7 @@ from db.models import Medida, MedidaResponse, Opcion, OpcionResponse, OpcionMedi
 from shared.dependencies import RoleChecker, SyncDbSessionDep, get_user_from_token_data
 from shared.enums import RolesEnum
 from shared.schemas import OpcionMedidaCreate, OpcionMedidaOut, UsuarioOut
-from shared.utils import get_example
+from shared.utils import get_example, get_local_now_datetime
 
 router = APIRouter(prefix="/opciones_medidas", tags=["Opciones Medidas"])
 
@@ -53,6 +53,7 @@ def read_opciones_medidas(
 )
 def add_opcion_medida(
     db: SyncDbSessionDep,
+    user: Annotated[UsuarioOut, Depends(get_user_from_token_data)],
     opcion_medida: OpcionMedidaCreate = Body(
         openapi_examples={
             "default": get_example("opcion_medida_post"),
@@ -81,26 +82,31 @@ def add_opcion_medida(
     )
     if not medida:
         raise HTTPException(status_code=404, detail="Medida no existe")
+    
+    if medida.tipo_dato.tipo_dato != "Selección":
+        raise HTTPException(status_code=400, detail="Solo se pueden registrar opciones para medidas con tipo de dato 'Selección'")
+
     if (
-        db.query(OpcionMedida)
+        db.query(OpcionMedidaResponse)
         .filter(
-            OpcionMedida.id_opcion == opcion_medida.id_opcion,
-            OpcionMedida.id_medida == opcion_medida.id_medida,
+            OpcionMedidaResponse.id_opcion == opcion_medida.id_opcion,
+            OpcionMedidaResponse.id_medida == opcion_medida.id_medida,
         )
         .first()
     ):
         raise HTTPException(status_code=409, detail="Opcion de medida ya existe")
 
-    opcion_medida = OpcionMedida(
-        id_opcion=opcion_medida.id_opcion, id_medida=opcion_medida.id_medida
+    opcion_medida = OpcionMedidaResponse(
+        id_opcion=opcion_medida.id_opcion, 
+        id_medida=opcion_medida.id_medida, 
+        fecha_creacion=get_local_now_datetime(),
+        creado_por=user.email
     )
     db.add(opcion_medida)
     db.commit()
     db.refresh(opcion_medida)
-    opcion_medida_out = OpcionMedidaOut(
-        id_opcion_medida=opcion_medida.id_opcion_medida, opcion=opcion, medida=medida
-    )
-    return {"message": "Se creó opcion de medida", "opcion_medida": opcion_medida_out}
+#    opcion_medida_out = OpcionMedidaOut(id_opcion_medida=opcion_medida.id_opcion_medida, opcion=opcion, medida=medida)
+    return {"message": "Se agregeó la opcion de medida", "opcion_medida": opcion_medida}
 
 
 @router.delete(
